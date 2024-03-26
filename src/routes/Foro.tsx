274b1ac040712fd43dce4f7927c29967ef28abbd
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-26 17:00:14 -0500

    feat: basic forum layout

--- src/routes/Foro.tsx
+++ src/routes/Foro.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Foro() {
   return (
-    <div>Foro</div>
+    <div className="p-16">
+      <div className="flex flex-row">
+        <Input type="text" placeholder="Buscar posts" className="mr-8"/>
+        <Button variant={"default"}>Crear Post</Button>
+      </div>
+    </div>
   )
 }
