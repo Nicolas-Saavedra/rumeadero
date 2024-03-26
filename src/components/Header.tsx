9ae639321144fba42aff42d38be0233618ef8e11
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-26 14:26:51 -0500

    feat: header page setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,29 @@
 import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
-import { Users, CarFront, GraduationCap, HandCoins, Store, Rat } from "lucide-react"
+import { Users, CarFront, GraduationCap, HandCoins, Store, MessageCircleMore, Martini } from "lucide-react"
 
 export default function Header() {
   
   return (
-    <nav className="sticky w-56 h-screen flex text-orange-900 flex-col bg-yellow-500 px-2">
-      <Link className="flex text-xl font-bold justify-center mt-4 mb-8" to={"/"}>El Raton<Rat className="ml-2"/></Link> 
-      <Button className="justify-start" asChild>
-        <Link className="rounded text-lg py-6 hover:bg-yellow-400" to={"/grupos"}><Users className="mr-2 ml-4"/>Grupos</Link> 
+    <nav className="sticky w-64 h-screen flex text-stone-800 flex-col bg-stone-50 px-2 border-stone-100 border-r-2">
+      <Link className="flex text-xl font-bold justify-center mt-4 mb-8" to={"/"}>El R<span className="text-yellow-500">u</span>meadero<Martini className="ml-2"/></Link> 
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-200" to={"/foro"}><MessageCircleMore className="mr-2 ml-4"/>Foro</Link> 
       </Button>
-      <Button className="justify-start" asChild>
-        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-400" to={"/wheels"}><CarFront className="mr-2 ml-4"/>Wheels</Link> 
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-lg py-6 hover:bg-yellow-200" to={"/grupos"}><Users className="mr-2 ml-4"/>Grupos</Link> 
       </Button>
-      <Button className="justify-start" asChild>
-        <Link className="rounded text-md py-6 mt-2 hover:bg-yellow-400" to={"/emprendimientos"}><Store className="mr-2 ml-4"/>Emprendimientos</Link> 
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-200" to={"/wheels"}><CarFront className="mr-2 ml-4"/>Wheels</Link> 
       </Button>
-      <Button className="justify-start" asChild>
-        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-400" to={"/ventas"}><HandCoins className="mr-2 ml-4"/>Ventas</Link> 
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-md py-6 mt-2 hover:bg-yellow-200" to={"/emprendimientos"}><Store className="mr-2 ml-4"/>Emprendimientos</Link> 
       </Button>
-      <Button className="justify-start" asChild>
-        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-400" to={"/tutorias"}><GraduationCap className="mr-2 ml-4"/>Tutorias</Link> 
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-200" to={"/ventas"}><HandCoins className="mr-2 ml-4"/>Ventas</Link> 
+      </Button>
+      <Button className="justify-start" asChild variant={"ghost"}>
+        <Link className="rounded text-lg py-6 mt-2 hover:bg-yellow-200" to={"/tutorias"}><GraduationCap className="mr-2 ml-4"/>Tutorias</Link> 
       </Button>
     </nav>
   )
