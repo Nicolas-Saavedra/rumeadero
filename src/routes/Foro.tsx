8790c03b6a57797043e7506d903c6074a1f61236
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-26 17:10:09 -0500

    feat: change component structure

--- src/routes/Foro.tsx
+++ src/routes/Foro.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import ForumHeader from "@/components/forum/ForumHeader";
 
 export default function Foro() {
   return (
     <div className="p-16">
-      <div className="flex flex-row">
-        <Input type="text" placeholder="Buscar posts" className="mr-8"/>
-        <Button variant={"default"}>Crear Post</Button>
-      </div>
+      <ForumHeader/>
     </div>
   )
 }
