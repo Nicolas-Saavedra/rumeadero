7c674ca4d9f15d8b56860a4805951e4ed79f4229
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-26 17:20:03 -0500

    chore: naming conventions

--- src/components/forum/ForumHeader.tsx
+++ src/components/forum/ForumHeader.tsx
@@ -1,5 +1,5 @@
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { Input } from "../ui/Input";
+import { Button } from "../ui/Button";
 
 export default function ForumHeader() {
   return (
