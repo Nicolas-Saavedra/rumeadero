1b12a2627894feab91635513995b88c0e6677bf7
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-27 01:39:16 -0500

    feat: added info button to clarify how page works

--- src/components/forum/ForumHeader.tsx
+++ src/components/forum/ForumHeader.tsx
@@ -1,11 +1,17 @@
 import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
+import { Info } from "lucide-react";
 
 export default function ForumHeader() {
   return (
     <div className="flex flex-row">
-      <Input type="text" placeholder="Buscar posts" className="mr-8" />
-      <Button variant={"default"}>Crear Post</Button>
+      <Input type="text" placeholder="Buscar posts..." className="mr-4" />
+      <Button className="mr-4 duration-300" variant={"default"}>
+        Crear Post
+      </Button>
+      <button>
+        <Info className="text-slate-600 hover:scale-90 duration-300" />
+      </button>
     </div>
   );
 }
