0978c09a88f46331b0ccd872c5a08f3c514687ae
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-27 22:54:07 -0500

    fix: place the onClick properly

--- src/components/forum/ForumHeader.tsx
+++ src/components/forum/ForumHeader.tsx
@@ -1,12 +1,18 @@
 import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
 import { Info } from "lucide-react";
+import { useDialogSet } from "@/context/dialogContext";
 
 export default function ForumHeader() {
+  const setDialog = useDialogSet();
   return (
     <div className="flex flex-row">
       <Input type="text" placeholder="Buscar posts..." className="mr-4" />
-      <Button className="mr-4 duration-300" variant={"default"}>
+      <Button
+        onClick={() => setDialog("login")}
+        className="mr-4 duration-300"
+        variant={"default"}
+      >
         Crear Post
       </Button>
       <button>
