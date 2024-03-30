d0631bade9c35f3fb57398497983b6db5e7d8ed6
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-30 12:58:23 -0500

    feat: change the ui for post to be more apropiate for app

--- src/components/forum/ForumHeader.tsx
+++ src/components/forum/ForumHeader.tsx
@@ -1,13 +1,70 @@
 import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
-import { Info } from "lucide-react";
+import { Flame, Info, Sparkles, TrendingUp } from "lucide-react";
 import { useDialogSet } from "@/context/dialogContext";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/DropdownMenu";
+import { useState } from "react";
+
+type FilterState = "hot" | "trending" | "new";
 
 export default function ForumHeader() {
   const setDialog = useDialogSet();
+  const [filterState, setFilterState] = useState<FilterState>("new");
+
+  let triggerDisplayTag;
+
+  switch (filterState) {
+    case "hot":
+      triggerDisplayTag = hotDisplayTag;
+      break;
+    case "trending":
+      triggerDisplayTag = trendingDisplayTag;
+      break;
+    case "new":
+      triggerDisplayTag = newDisplayTag;
+      break;
+  }
+
   return (
     <div className="flex flex-row">
-      <Input type="text" placeholder="Buscar posts..." className="mr-4" />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="w-36 mr-4" variant={"outline"}>
+            {triggerDisplayTag}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40">
+          <DropdownMenuLabel>Mostrar por</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={filterState}
+            onValueChange={setFilterState as (value: string) => void}
+          >
+            <DropdownMenuRadioItem value="hot">
+              {hotDisplayTag}
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="trending">
+              {trendingDisplayTag}
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="new">
+              {newDisplayTag}
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Input
+        type="text"
+        placeholder="Filtrar posts por tag..."
+        className="mr-4"
+      />
       <Button
         onClick={() => setDialog("login")}
         className="mr-4 duration-300"
@@ -21,3 +78,20 @@ export default function ForumHeader() {
     </div>
   );
 }
+const hotDisplayTag = (
+  <div className="text-red-400 flex flex-row">
+    <Flame className="mr-2" /> <span>Caliente</span>
+  </div>
+);
+
+const trendingDisplayTag = (
+  <div className="text-yellow-400 flex flex-row">
+    <TrendingUp className="mr-2" /> <span>Tendencia</span>
+  </div>
+);
+
+const newDisplayTag = (
+  <div className="text-blue-400 flex flex-row">
+    <Sparkles className="mr-2" /> <span>Nuevo</span>
+  </div>
+);
