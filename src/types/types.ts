baa895688dab13c34957ffb28a4062d507fab247
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-30 16:55:11 -0500

    feat: working out types

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,11 +8,13 @@ export type ForumPost = {
   imageAlt?: string;
 };
 
-export type Group = {
+export type GroupPreview = {
   owner?: string;
   name: string;
   description: string;
   numberMembers: string;
   tags: string[];
+  primarySocialMedia: string;
+  socialMedia: string[];
   imageUrl: string;
 };
