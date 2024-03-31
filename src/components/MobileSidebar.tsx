81ac4ba4b1720f7fc75015e4bf5f98bd2b2d4059
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-03-31 13:47:46 -0500

    feat: mobile ui wip navbar

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -15,58 +15,52 @@ import { useDialogSet } from "@/context/dialogContext";
 export default function MobileSidebar() {
   const setDialog = useDialogSet();
   return (
-    <div className="sticky w-24 h-screen flex text-stone-800 flex-col justify-between px-2">
+    <div className="sticky w-16 sm:w-24 h-screen flex text-stone-800 flex-col justify-between px-0 md:px-2">
       <div className="flex flex-col ml-4">
         <Link
-          className="flex text-xl md:text-2xl font-bold justify-center mt-4 mb-8 items-center"
+          className="flex text-xl md:text-2xl font-bold justify-center mt-9 mb-8 items-center"
           to={"/"}
         >
           <Martini />
         </Link>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link className="text-lg md:text-xl py-6 mt-2" to={"/foro"}>
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="md:text-xl py-4 mt-3" to={"/foro"}>
             <MessageCircleMore />
           </Link>
         </Button>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link className="text-lg md:text-xl py-6" to={"/grupos"}>
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="md:text-xl py-4 mt-4" to={"/grupos"}>
             <Users />
           </Link>
         </Button>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link className="text-lg md:text-xl py-6 mt-2" to={"/wheels"}>
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="md:text-xl py-4 mt-4" to={"/wheels"}>
             <CarFront />
           </Link>
         </Button>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link
-            className="text-lg md:text-xl py-6 mt-2"
-            to={"/emprendimientos"}
-          >
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="md:text-xl py-4 mt-4" to={"/emprendimientos"}>
             <Store />
           </Link>
         </Button>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link className="text-lg md:text-xl py-6 mt-2" to={"/ventas"}>
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="md:text-xl py-4 mt-4" to={"/ventas"}>
             <HandCoins />
           </Link>
         </Button>
-        <Button className="justify-center" asChild variant={"ghost"}>
-          <Link
-            className="text-lg text-center md:text-xl py-6 mt-2"
-            to={"/tutorias"}
-          >
+        <Button className="justify-center p-0 md:p-4" asChild variant={"ghost"}>
+          <Link className="text-center md:text-xl py-4 mt-4" to={"/tutorias"}>
             <GraduationCap />
           </Link>
         </Button>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col ml-4">
         <Button
-          className="justify-center text-md py-6 my-4"
+          className="justify-center my-4 p-0 md:p-4"
           onClick={() => setDialog("login")}
           variant={"ghost"}
         >
-          <CircleUser className="ml-4" />
+          <CircleUser />
         </Button>
       </div>
     </div>
