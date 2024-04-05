0373da8ab3c3b6b8c1eb2184f5913311d56a8acb
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-04-05 00:43:26 -0500

    fix: remove remains of firebase migration

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.css";
-import { FirebaseProvider } from "./context/firebaseContext.tsx";
 import { DialogProvider } from "./context/dialogContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FirebaseProvider>
-      <DialogProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </DialogProvider>
-    </FirebaseProvider>
+    <DialogProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DialogProvider>
   </React.StrictMode>,
 );
