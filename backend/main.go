40a74e137ecfd50d4a40abe44aa08028c635f8d9
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-05-01 18:03:33 -0500

    feat!: added listener to create private entry, untested

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,8 @@ import (
 	_ "github.com/Nicolas-Saavedra/rumeadero-backend/migrations"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/forms"
+	"github.com/pocketbase/pocketbase/models"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
@@ -34,6 +36,28 @@ func main() {
 			return err
 		}
 		return nil
+	})
+
+	app.OnModelAfterCreate("users").Add(func(e *core.ModelEvent) error {
+		collection, err := e.Dao.FindCollectionByNameOrId("users_private")
+		if err != nil {
+			return err
+		}
+		record := models.NewRecord(collection)
+		form := forms.NewRecordUpsert(app, record)
+
+		err = form.LoadData(map[string]any{
+			"direction": nil,
+			"user":      e.Model.GetId(),
+			"settings": map[string]any{
+				"providers": []string{},
+				"theme":     "light",
+			},
+		})
+		if err != nil {
+			return err
+		}
+		return form.Submit()
 	})
 
 	if err := app.Start(); err != nil {
